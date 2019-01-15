d73099a30314000a31b15e67ffb9e2ff93a742e1
Yuri Buyanov <yuri.buyanov@gmail.com>
2019-01-15 13:17:03 +0300

    deps and stuff

--- Sources/App/Models/Subscription.swift
+++ Sources/App/Models/Subscription.swift
@@ -1,24 +1,26 @@
-import FluentSQLite
+import Fluent
 import Vapor
 
-final class Subscription<D>: Model where D: Database {
-    typealias Database = D
-    
-    typealias ID = UUID
-    
-    var id: ID
-    var title: String? = nil
-    var url: String
-    var chatId: Int
-    var updated: Float
-    
-    /// Creates a new `Todo`.
-    init(url: String, chatId: Int) {
-        self.id = UUID()
-        self.updated = Date().timeIntervalSince1970()
-
-//        self.id = id
-        self.url = url
-        self.chatId = chatId
-    }
-}
+//final class Subscription: Model where D: Database {
+////    typealias Database = <#type#>
+//    
+//    
+//    typealias Database = D
+//    typealias ID = UUID
+//    
+//    var id: ID
+//    var title: String? = nil
+//    var url: String
+//    var chatId: Int
+////    var updated: Float
+//    
+//    /// Creates a new `Todo`.
+//    init(url: String, chatId: Int) {
+//        self.id = UUID()
+////        self.updated = Date().timeIntervalSince1970()
+//
+////        self.id = id
+//        self.url = url
+//        self.chatId = chatId
+//    }
+//}
