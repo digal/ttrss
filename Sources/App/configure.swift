0cdf85bad775666f8781696ebc0f8ad3d13f5c97
Yuri Buyanov <yuri.buyanov@gmail.com>
2019-01-15 15:32:13 +0300

    plsql creds

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,7 +4,7 @@ import Vapor
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
-//    try services.register(FluentPostgreSQLProvider(enableIdentityColumns: true))
+    try services.register(FluentPostgreSQLProvider(enableIdentityColumns: true))
     try services.register(CredentialsProvider(token: Environment.get("RSS_TT_TOKEN")!))
 
 
@@ -20,12 +20,13 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-//    let dbConfig = PostgreSQLDatabaseConfig(url: "")!
-//    let postgre = PostgreSQLDatabase(config: dbConfig)
-//
-//    /// Register the configured SQLite database to the database config.
-//    var databases = DatabasesConfig()
-//    databases.add(database: postgre, as: .psql)
-//    services.register(databases)
+    let dbUrl = Environment.get("DATABASE_URL") ?? ""
+    let dbConfig = PostgreSQLDatabaseConfig(url: dbUrl)!
+    let postgre = PostgreSQLDatabase(config: dbConfig)
+
+    /// Register the configured SQLite database to the database config.
+    var databases = DatabasesConfig()
+    databases.add(database: postgre, as: .psql)
+    services.register(databases)
 
 }
